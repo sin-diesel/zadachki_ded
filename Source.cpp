19c113d7c24e5f15bc92929cc489c42f1cc39888
Олег <suhodolov.o@yandex.ru>
2021-08-21 13:14:59 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <math.h>
-
+#include <assert.h>
+#include <limits.h>
 bool wasOverflow(double n)
 {
 	return isinf(n) || isnan(n);
@@ -18,6 +19,7 @@ bool equal(double a, double b)
 
 void getCoefficients(double* a, double* b, double* c)
 {
+	assert(a != nullptr && b != nullptr && c != nullptr);
 	printf("Enter coefficients of A*x^2 + B*x + C == 0\nA:");
 	
 	scanf("%lf", a);
@@ -27,54 +29,91 @@ void getCoefficients(double* a, double* b, double* c)
 	scanf("%lf", c);
 }
 
-void solveLinear(double a, double b)
+int solveLinear(double a, double b, double ** res)
 {
 	if (equal(a, 0.0))
 	{
 		if (equal(b, 0.0))
-			printf("Roots are all real numbers");
+			return INT_MAX;
 		else
-			printf("No roots");
+			return 0;
 
 	}
 	else
-		printf("x = %g", -b / a);
+	{
+		*res = new double[1];
+		**res = -b / a;
+		return 1;
+	}
 
 }
 
-void solveQuadratic(double a, double b, double c)
+int solveQuadratic(double a, double b, double c, double ** res)
 {
 	double b_half = b / 2;
-	double D_divided_by_4 = pow(b_half, 2) - a * c;
+	double D_divided_by_4 = b_half * b_half - a * c;
 	if (wasOverflow(D_divided_by_4))
 	{
-		printf("Overflow error");
+		return -1;
 	}
 	else if (equal(D_divided_by_4, 0.0))
 	{
-		printf("x = %g", -b_half / a);
+		*res = new double[1];
+		**res = -b_half / a;
+		return 1;
 	}
 	else if (D_divided_by_4 < 0)
 	{
-		printf("No roots");
+		return 0;
 	}
 	else
 	{
 		double sq_root = sqrt(D_divided_by_4);
-		printf("x1 = %g\n", (-b_half + sq_root) / a);
-		printf("x2 = %g", (-b_half - sq_root) / a);
+		*res = new double[2];
+		(*res)[0] = (-b_half + sq_root) / a;
+		(*res)[1] = (-b_half - sq_root) / a;
+		return 2;
 	}
+}
+
+int solveEquation(double a, double b, double c, double** res)
+{
+	assert(res != nullptr && *res == nullptr);
+	if (equal(a, 0.0))
+		return solveLinear(b, c, res);
+	return solveQuadratic(a, b, c, res);
+}
+
+void outResult(int amount_of_roots, double* roots)
+{
+	if (amount_of_roots == -1)
+		printf("Overflow error\n");
+	else if (amount_of_roots == 0)
+		printf("No roots\n");
+	else if (amount_of_roots == 1)
+		printf("x = %g\n", *roots);
+	else if (amount_of_roots == 2)
+		printf("x1 = %g\nx2 = %g\n", roots[0], roots[1]);
+	else if (amount_of_roots == INT_MAX)
+		printf("All reals are roots\n");
+	else
+		assert(false);
 }
 int main()
 {
 
-	double a = 0, b = 0, c = 0;
+	double a = nan(""), b = nan(""), c = nan("");
 	getCoefficients(&a, &b, &c);
-	if (equal(a, 0.0))
-		solveLinear(b, c);
-	else
-		solveQuadratic(a, b, c);
+	if (isnan(a) || isnan(b) || isnan(c))
+	{
+		printf("Error in input");
+		return 0;
+	}
 	
+	double * roots = nullptr;
+	int amount_of_roots = solveEquation(a, b, c, &roots);
+	outResult(amount_of_roots, roots);
+	delete[] roots;
 	getchar();
 
 }
